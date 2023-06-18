3e7187222b411263edf159b8115f9da75eda44e4
KeylorCruz <42794277+KeylorCruz@users.noreply.github.com>
2023-06-18 09:42:29 -0600

    Cambio en la visualizacion de MenuPrincipal.js

--- MenuPrincipal.js
+++ MenuPrincipal.js
@@ -20,9 +20,12 @@ function JuegoPrincipal() {
     return <MultiplayerAreaDeJuego />;
   } else {
     return (
-      <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' , backgroundColor: '#6E8DC0' }}>
-        <button className="btn btn-dark" onClick={handleSoloClick}>Jugar en solitario</button>
-        <button className="btn btn-dark" onClick={handleMultiplayerClick}>Jugar en multijugador</button>
+      <div className="d-flex flex-column justify-content-center align-items-center" style={{ minHeight: '100vh', backgroundColor: '#6E8DC0' }}>
+        <h2 className="mt-4">Match3 Game</h2>
+        <div className="d-grid gap-2 col-6 mx-auto mt-4">
+          <button className="btn btn-dark" onClick={handleSoloClick}>Jugar en solitario</button>
+          <button className="btn btn-dark" onClick={handleMultiplayerClick}>Jugar en multijugador</button>
+        </div>
       </div>
     );
   }
