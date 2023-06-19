961e97403156c20d46fbeba16a29ba354ab07e7c
Ch4var <83097741+Ch4var@users.noreply.github.com>
2023-06-18 19:02:14 -0600

    Actualizado Menú principal
    Cambios en visualización

--- MenuPrincipal.js
+++ MenuPrincipal.js
@@ -4,7 +4,7 @@ import MultiplayerAreaDeJuego from './MultiplayerAreaDeJuego';
 import './bootstrap.css';
 
 /*
-Funcion cuyo objetivo es cargar el archivo AreaDeJuego o MultiplayerAreaDeJuego segun la opcion seleccionada.
+Funcion principal del componente MenuPrincipal cuyo objetivo es cargar el archivo AreaDeJuego o MultiplayerAreaDeJuego segun la opcion seleccionada.
 No tiene entradas ni salidas, solo toma el modo de juego que el usuario selecciona al presionar el respectivo boton
 */
 function JuegoPrincipal() {
@@ -24,9 +24,12 @@ function JuegoPrincipal() {
     return <MultiplayerAreaDeJuego />;
   } else {
     return (
-      <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' , backgroundColor: '#6E8DC0' }}>
-        <button className="btn btn-dark" onClick={handleSoloClick}>Jugar en solitario</button>
-        <button className="btn btn-dark" onClick={handleMultiplayerClick}>Jugar en multijugador</button>
+      <div className="d-flex flex-column justify-content-center align-items-center" style={{ minHeight: '100vh', backgroundColor: '#6E8DC0' }}>
+        <h2 className="mt-4">Match3 Game</h2>
+        <div className="d-grid gap-2 col-6 mx-auto mt-4">
+          <button className="btn btn-dark" onClick={handleSoloClick}>Jugar en solitario</button>
+          <button className="btn btn-dark" onClick={handleMultiplayerClick}>Jugar en multijugador</button>
+        </div>
       </div>
     );
   }
